b95e03deae63d79140787875d52fad67858750ea
basil-balthasar <batabasil@hotmail.com>
2024-08-09 18:17:59 +0200

    image file created

--- frontend/image.js
+++ frontend/image.js
@@ -1,5 +1,9 @@
-let numberOfImages = [2,2]
+let numberOfImages = [2, 0, 2, 0]
 
+let backgroundImages = []
+let foregroundImages = []
+let layerOneImages = []
+let layerTwoImages = []
 
 class ImageObject{
     constructor(isOn, image, position, scale, rotation, blendMode){
@@ -9,5 +13,20 @@ class ImageObject{
         this.scale = scale
         this.rotation = rotation
         this.blendMode = blendMode
+    }
+}
+
+function preload(){
+    for(let b = 0; b<numberOfImages[0]; b++){
+        backgroundImages[b] = loadImage("./content/collageElements/BG/"+(b).toString()+".png")
+    }
+    for(let f = 0; f<numberOfImages[1]; f++){
+        foregroundImages[f] = loadImage("./content/collageElements/FG/"+(f).toString()+".png")
+    }
+    for(let o = 0; o<numberOfImages[2]; o++){
+        layerOneImages[o] = loadImage("./content/collageElements/ONE/"+(o).toString()+".png")
+    }
+    for(let t = 0; t<numberOfImages[3]; t++){
+        layerTwoImages[t] = loadImage("./content/collageElements/TWO/"+(t).toString()+".png")
     }
 }
