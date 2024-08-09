cb9afaed0931566161d3acab5fbf7b7e0d62ebcb
basil-balthasar <batabasil@hotmail.com>
2024-08-09 22:24:40 +0200

    image loading

--- frontend/image.js
+++ frontend/image.js
@@ -1,9 +1,9 @@
 let numberOfImages = [2, 0, 2, 0]
 
-let backgroundImages = []
-let foregroundImages = []
-let layerOneImages = []
-let layerTwoImages = []
+let backgroundImages = new ImageObject(true, backgroundImages[0], createVector(0,0) , 1.0, 0.0, 0);
+let foregroundImages = new ImageObject(true, foregroundImages[0], createVector(0,0), 1.0, 0.0, 0);
+let layerOneImages = new ImageObject(true, layerOneImages[0], createVector(0,0), 0.1, 45.0, 0);
+let layerTwoImages =  new ImageObject(false, layerTwoImages[0], createVector(0,0), 1.0, 0.0, 0);
 
 class ImageObject{
     constructor(isOn, image, position, scale, rotation, blendMode){
@@ -17,16 +17,20 @@ class ImageObject{
 }
 
 function preload(){
-    for(let b = 0; b<numberOfImages[0]; b++){
+    try{
+        for(let b = 0; b<numberOfImages[0]; b++){
         backgroundImages[b] = loadImage("./content/collageElements/BG/"+(b).toString()+".png")
-    }
-    for(let f = 0; f<numberOfImages[1]; f++){
-        foregroundImages[f] = loadImage("./content/collageElements/FG/"+(f).toString()+".png")
-    }
-    for(let o = 0; o<numberOfImages[2]; o++){
-        layerOneImages[o] = loadImage("./content/collageElements/ONE/"+(o).toString()+".png")
-    }
-    for(let t = 0; t<numberOfImages[3]; t++){
-        layerTwoImages[t] = loadImage("./content/collageElements/TWO/"+(t).toString()+".png")
+        }
+        for(let f = 0; f<numberOfImages[1]; f++){
+            foregroundImages[f] = loadImage("./content/collageElements/FG/"+(f).toString()+".png")
+        }
+        for(let o = 0; o<numberOfImages[2]; o++){
+            layerOneImages[o] = loadImage("./content/collageElements/ONE/"+(o).toString()+".png")
+        }
+        for(let t = 0; t<numberOfImages[3]; t++){
+            layerTwoImages[t] = loadImage("./content/collageElements/TWO/"+(t).toString()+".png")
+        }
+    }catch(err){
+        console.log(err)
     }
 }
