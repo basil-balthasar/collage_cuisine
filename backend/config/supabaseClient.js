9f06e2a4897a366af31c8dbe3d947761ad76af04
lucabusby <128382812+lucabusby@users.noreply.github.com>
2024-11-07 17:22:23 +0100

    Changed Supabase Acc

--- backend/config/supabaseClient.js
+++ backend/config/supabaseClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = "https://guuydhldgjexrbvozxeb.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1dXlkaGxkZ2pleHJidm96eGViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMyMDc2MzksImV4cCI6MjAzODc4MzYzOX0.ePkhw7kWV1M3Tv8QwihpU7IWeKJYWV6ZYZDDrdj9viE"
+const supabaseUrl = "https://pdsrdvuhphvnqybxzmkt.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkc3JkdnVocGh2bnF5Ynh6bWt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5OTYzNDgsImV4cCI6MjA0NjU3MjM0OH0.FjKwkuBU9C96TmaTgMGaxXDx6rOy6IC0CDS6F-UEscw"
 
 if (!supabaseUrl || !supabaseKey) {
     console.error("Missing Supabase URL or anon key. Check your .env file.");
